921ae3ed20a3ea61f157612b7afca65c3f8fd6fd
Papun Mohanty <papun.mohanty@homerload.com>
2023-02-23 07:36:24 +0530

    Fix API Test cases

--- django_core/core/api/tests/test_example.py
+++ django_core/core/api/tests/test_example.py
@@ -9,16 +9,14 @@ def test_list_view_stuffs():
 
     request = factory.get("/api/list/")
 
-    with patch("core.api.services.invoke_third_party_api") as mock_invoke_api:
+    with patch("core.api.views.invoke_third_party_api") as mock_invoke_api:
         mock_invoke_api.return_value = {
-            "message": {
-                "location_data": {
-                    "location_id": 1,
-                    "location_name": "Sample Location 1",
-                    "address": "Address 1, for Shipment",
-                    "latitude": 2.13,
-                    "longitude": 7.89
-                }
+            "location_data": {
+                "location_id": 1,
+                "location_name": "Sample Location 1",
+                "address": "Address 1, for Shipment",
+                "latitude": 2.13,
+                "longitude": 7.89
             }
         }
         view = ListViewStuffs.as_view()
--- django_core/core/api/tests/test_example2.py
+++ django_core/core/api/tests/test_example2.py
@@ -9,8 +9,9 @@ from core.api.views import ListViewStuffs
 
 @pytest.fixture
 def mocked_invoke_third_party_api():
-    with patch("core.api.services.invoke_third_party_api") as mock_invoke:
+    with patch("core.api.views.invoke_third_party_api") as mock_invoke:
         yield mock_invoke
+
 
 def test_list_view_stuff2(mocked_invoke_third_party_api):
     factory = APIRequestFactory()
@@ -26,11 +27,19 @@ def test_list_view_stuff2(mocked_invoke_third_party_api):
             }
         }
     }
-    mocked_invoke_third_party_api.return_value = listings
+    mocked_invoke_third_party_api.return_value =  {
+            "location_data": {
+                "location_id": 1,
+                "location_name": "Sample Location 1",
+                "address": "Address 1, for Shipment",
+                "latitude": 2.13,
+                "longitude": 7.89
+            }
+        }
 
     view = ListViewStuffs.as_view()
-    with patch('core.api.services.invoke_third_party_api', mocked_invoke_third_party_api): 
-        response = view(request)
+    response = view(request)
     print(f"Response2: {response.data}")
+    print(f"listings: {listings}")
     assert response.status_code == 200
-    assert response.json() == listings
+    assert response.data==listings
